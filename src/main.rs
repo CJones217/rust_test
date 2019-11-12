a1a548a78d7074d3510a0f17c62ab0c990077979
Collin Jones <cjones28@luc.edu>
2019-11-12 17:43:53 -0600

    simple for loop and if else practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    let mut num8 = 0;
+    let mut num2 =0;
+    for n in 1..101 {
+        if n %2 ==0 {
+            num8 +=1;
+        }
+        else{
+            num2 +=1;
+        }
+    }
+    println!("{}",num8);
+    println!("{}",num2);
 }
